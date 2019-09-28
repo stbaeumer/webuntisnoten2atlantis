b9f4380e36daba8693561931781ef9a4229d760c
Stu <stbaeumer@gmail.com>
2019-09-28 07:33:25 +0200

    ...

--- webuntisnoten2atlantis/Program.cs
+++ webuntisnoten2atlantis/Program.cs
@@ -10,6 +10,17 @@ namespace webuntisnoten2atlantis
     {
         static void Main(string[] args)
         {
+            List<string> aktSj = new List<string>
+            {
+                (DateTime.Now.Month >= 8 ? DateTime.Now.Year : DateTime.Now.Year - 1).ToString(),
+                (DateTime.Now.Month >= 8 ? DateTime.Now.Year + 1 : DateTime.Now.Year).ToString()
+            };
+
+            Console.WriteLine("Webuntisnoten2atlantis (Version 20190914)");
+            Console.WriteLine("====================================");
+            Console.WriteLine("");
+
+
         }
     }
 }
